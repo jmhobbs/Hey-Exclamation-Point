76e9fc47b243f4dfaeafe6178c30b4246796b296
John Hobbs <john@velvetcache.org>
2010-01-20 23:32:13 -0600

    More conceptual work, a little bit on the loader.

--- Hey.py
+++ Hey.py
@@ -11,22 +11,46 @@ def full_path ( relpath ):
 	"""
 	return sys.path[0] + '/' + relpath
 
+class Day:
+	def __init__ ( self ):
+		return
+
 class Reminder:
 	"""
 	Defines a reminder.
 	"""
-	def __init__ ( self ):
+	def __init__ ( self, path=None ):
 		self.name = 'UNDEFINED'
+		self.text = 'UNDEFINED'
 		self.exceptions = []
 		self.days = []
 
+		if None != yaml:
+			self.load_from_file( path )
+
 	def load_from_file ( self, path ):
 		# Open file, load yaml
-		f = open( root + '/' + file )
+		f = open( path )
 		raw = yaml.load( f )
 		f.close()
+		self.name = raw['reminder']['name']
+		self.text = raw['reminder']['text']
+		for day in raw['reminder']['days']:
+			if 'monday' == day.lower():
+			elif 'tuesday' == day.lower():
+			elif 'wednesday' == day.lower():
+			elif 'thursday' == day.lower():
+			elif 'friday' == day.lower():
+			elif 'saturday' == day.lower():
+			elif 'sunday' == day.lower():
 
 if __name__ == "__main__":
+
+	reminders = {}
+
 	import os
-	for root, dirs, files in os.walk( full_path( 'reminders/' ) ):
+
+	for root, dirs, files in os.walk( full_path( 'reminders/on/' ) ):
 			for file in files:
+				if '.yaml' == file[-5:]:
+					reminders[file[-5:]] = Reminder( root + file )
